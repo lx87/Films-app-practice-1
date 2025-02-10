3985e91f673af389ff0bd38a6c86eedb6b26ef80
lx87 <limarx87@gmail.com>
2025-02-10 23:48:03 +0300

    alignment fix

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,23 +3,25 @@ import Movie from './Movie';
 
 const Movies = ({ movies, hasSearched }) => {
   return (
-    <div className="row cards">
-      {movies.length ? (
-        movies.map((movie) => (
-          <div key={movie.imdbID} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <Movie movie={movie} />
-          </div>
-        ))
-      ) : ( 
-        hasSearched && (
-          <div className='v-stack align-items-center justify-content-center not-found' id='error'>
-              <i className="bi bi-4-square me-2 fs-1"></i>
-              <i className="bi bi-0-square me-2 fs-1"></i>
-              <i className="bi bi-4-square fs-1"></i>
-              <h1 className='mt-2'>Not found!</h1>
-          </div>
-        )
-       )}
+    <div className='container-fluid d-flex flex-column align-items-center'>
+      <div className="row cards">
+        {movies.length ? (
+          movies.map((movie) => (
+            <div key={movie.imdbID} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+              <Movie movie={movie} />
+            </div>
+          ))
+        ) : ( 
+          hasSearched && (
+            <div className='v-stack align-items-center justify-content-center not-found' id='error'>
+                <i className="bi bi-4-square me-2 fs-1"></i>
+                <i className="bi bi-0-square me-2 fs-1"></i>
+                <i className="bi bi-4-square fs-1"></i>
+                <h1 className='mt-2'>Not found!</h1>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 };
